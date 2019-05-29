0804dbf36323f4b87a20cd593faa87892057ed7e
unknown <myrnamares90@gmail.com>
2019-05-29 13:31:41 -0500

    Se creo el componente que contiene el splash con la redirección a la pagina de Home

--- foodmap/src/components/Home.js
+++ foodmap/src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
     render(){
         return(
             <div>
-                <h1>Home</h1>
+                <h1>Hello</h1>
             </div>
         )
     }
